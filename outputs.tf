2ab56070ae67e83736692ce9f0725429e484ab78
bangjur <azzuriptr@gmail.com>
2025-01-08 14:50:25 +0700

    update

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,15 @@ output "ec2_ip_list" {
     dbserver_priv_ips = [aws_instance.db_server1.private_ip]
   }
 }
+
+data "template_file" "nginx_default_conf" {
+  template = file("./nginx_default_conf.tmpl")
+  vars = {
+    alb_dns_name = aws_lb.app_lb.dns_name
+  }
+}
+
+resource "local_file" "nginx_default_conf" {
+  content  = data.template_file.nginx_default_conf.rendered
+  filename = "./playbooks/default.conf"
+}
